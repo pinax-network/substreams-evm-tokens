0b5b668e604dedb7d6377013280a26f5088977a1
Denis Carriere <carriere.denis@gmail.com>
2025-05-05 19:20:31 -0400

    Make ERC-1155 RPC Poller compatible

--- erc1155/src/lib.rs
+++ erc1155/src/lib.rs
@@ -1,6 +1,7 @@
-use substreams_ethereum::pb::eth::v2::Block;
-use proto::pb::evm::erc1155::v1::{TransferSingle, TransferBatch, ApprovalForAll, Events, Uri};
+use common::logs_with_caller;
+use proto::pb::evm::erc1155::v1::{ApprovalForAll, Events, TransferBatch, TransferSingle, Uri};
 use substreams_abis::evm::token::erc1155::events as erc1155;
+use substreams_ethereum::pb::eth::v2::Block;
 use substreams_ethereum::Event;
 
 #[substreams::handlers::map]
@@ -8,9 +9,7 @@ fn map_events(block: Block) -> Result<Events, substreams::errors::Error> {
     let mut events = Events::default();
 
     for trx in block.transactions() {
-        for (log, call_view) in trx.logs_with_calls() {
-            let call = call_view.call;
-
+        for (log, caller) in logs_with_caller(&block, trx) {
             // -- TransferSingle --
             if let Some(event) = erc1155::TransferSingle::match_and_decode(log) {
                 events.transfers_single.push(TransferSingle {
@@ -18,7 +17,7 @@ fn map_events(block: Block) -> Result<Events, substreams::errors::Error> {
                     tx_hash: trx.hash.to_vec(),
 
                     // -- call --
-                    caller: Some(call.caller.to_vec()),
+                    caller: caller.clone(),
 
                     // -- log --
                     ordinal: log.ordinal,
@@ -28,7 +27,7 @@ fn map_events(block: Block) -> Result<Events, substreams::errors::Error> {
                     operator: event.operator.to_vec(),
                     from: event.from.to_vec(),
                     to: event.to.to_vec(),
-                    id: event.id.to_string(), // uint256
+                    id: event.id.to_string(),       // uint256
                     value: event.value.to_string(), // uint256
                 });
             }
@@ -39,7 +38,7 @@ fn map_events(block: Block) -> Result<Events, substreams::errors::Error> {
                     tx_hash: trx.hash.to_vec(),
 
                     // -- call --
-                    caller: Some(call.caller.to_vec()),
+                    caller: caller.clone(),
 
                     // -- log --
                     ordinal: log.ordinal,
@@ -49,7 +48,7 @@ fn map_events(block: Block) -> Result<Events, substreams::errors::Error> {
                     operator: event.operator.to_vec(),
                     from: event.from.to_vec(),
                     to: event.to.to_vec(),
-                    ids: event.ids.iter().map(|id| id.to_string()).collect(), // uint256[]
+                    ids: event.ids.iter().map(|id| id.to_string()).collect(),             // uint256[]
                     values: event.values.iter().map(|value| value.to_string()).collect(), // uint256[]
                 });
             }
@@ -60,7 +59,7 @@ fn map_events(block: Block) -> Result<Events, substreams::errors::Error> {
                     tx_hash: trx.hash.to_vec(),
 
                     // -- call --
-                    caller: Some(call.caller.to_vec()),
+                    caller: caller.clone(),
 
                     // -- log --
                     ordinal: log.ordinal,
@@ -80,7 +79,7 @@ fn map_events(block: Block) -> Result<Events, substreams::errors::Error> {
                     tx_hash: trx.hash.to_vec(),
 
                     // -- call --
-                    caller: Some(call.caller.to_vec()),
+                    caller,
 
                     // -- log --
                     ordinal: log.ordinal,
