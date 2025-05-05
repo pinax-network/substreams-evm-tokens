c01a5ee6d9fdc43e1c93c52ab4b6cf5bc2c33ae2
Denis Carriere <carriere.denis@gmail.com>
2025-05-05 12:26:38 -0400

    Add batch

--- erc721-metadata/src/calls.rs
+++ erc721-metadata/src/calls.rs
@@ -1,17 +1,80 @@
-use substreams::scalar::BigInt;
+use std::{collections::HashMap, str::FromStr};
+
+use common::Address;
+use substreams::{scalar::BigInt, Hex};
 use substreams_abis::evm::token::erc721;
+use substreams_ethereum::rpc::RpcBatch;
+
+static CHUNK_SIZE: usize = 50;
 
 // Returns the token collection symbol.
-pub fn get_token_collection_symbol(contract: Vec<u8>) -> Option<String> {
+pub fn _call_token_collection_symbol(contract: Address) -> Option<String> {
     erc721::functions::Symbol {}.call(contract)
 }
 
 // Returns the token collection name.
-pub fn get_token_collection_name(contract: Vec<u8>) -> Option<String> {
+pub fn _call_token_collection_name(contract: Address) -> Option<String> {
     erc721::functions::Name {}.call(contract)
 }
 
 // Returns the Uniform Resource Identifier (URI) for `tokenId` token.
-pub fn get_token_uri(contract: Vec<u8>, token_id: BigInt) -> Option<String> {
+pub fn _call_token_uri(contract: Address, token_id: BigInt) -> Option<String> {
     erc721::functions::TokenUri{token_id}.call(contract)
+}
+
+// Returns the token collection name.
+pub fn batch_token_collection_name(contracts: Vec<Address>) -> HashMap<Address, String> {
+    let mut results: HashMap<Address, String> = HashMap::new();
+    for chunks in contracts.chunks(CHUNK_SIZE) {
+        let batch = chunks.iter().fold(RpcBatch::new(), |batch, address| {
+            batch.add(erc721::functions::Name {}, address.to_vec())
+        });
+        let responses = batch.execute().expect("failed to execute erc721::functions::Name RpcBatch").responses;
+        for (i, address) in chunks.iter().enumerate() {
+            if let Some(name) = RpcBatch::decode::<String, erc721::functions::Name>(&responses[i]) {
+                results.insert(address.to_vec(), name);
+            } else {
+                substreams::log::info!("Failed to decode erc721::functions::Name for address={:?}", Hex::encode(address));
+            }
+        }
+    }
+    results
+}
+
+pub fn batch_token_collection_symbol(contracts: Vec<Address>) -> HashMap<Address, String> {
+    let mut results: HashMap<Address, String> = HashMap::new();
+    for chunks in contracts.chunks(CHUNK_SIZE) {
+        let batch = chunks.iter().fold(RpcBatch::new(), |batch, address| {
+            batch.add(erc721::functions::Symbol {}, address.to_vec())
+        });
+        let responses = batch.execute().expect("failed to execute erc721::functions::Symbol RpcBatch").responses;
+        for (i, address) in chunks.iter().enumerate() {
+            if let Some(symbol) = RpcBatch::decode::<String, erc721::functions::Symbol>(&responses[i]) {
+                results.insert(address.to_vec(), symbol);
+            } else {
+                substreams::log::info!("Failed to decode erc721::functions::Symbol for address={:?}", Hex::encode(address));
+            }
+        }
+    }
+    results
+}
+
+// Returns the token URI.
+pub fn batch_token_uri(token_ids: Vec<(Address, String)>) -> HashMap<(Address, String), String> {
+    let mut results: HashMap<(Address, String), String> = HashMap::with_capacity(token_ids.len());
+
+    for chunk in token_ids.chunks(CHUNK_SIZE) {
+        let batch = chunk.iter().fold(RpcBatch::new(), |batch, (address, id)| {
+            batch.add(erc721::functions::TokenUri { token_id: BigInt::from_str(id).unwrap() }, address.to_vec())
+        });
+        let responses = batch.execute().expect("failed to execute erc721::functions::TokenUri batch").responses;
+        for (i, (address, token_id)) in chunk.iter().enumerate() {
+            if let Some(uri) = RpcBatch::decode::<String, erc721::functions::TokenUri>(&responses[i]) {
+                results.insert((address.to_vec(), token_id.to_string()), uri.trim_end_matches('\0').to_string());
+            } else {
+                substreams::log::info!("Failed to decode TokenUri for address={:?} token_id={:?}", Hex::encode(address), token_id);
+            }
+        }
+    }
+    results
 }
--- erc721-metadata/src/maps.rs
+++ erc721-metadata/src/maps.rs
@@ -1,25 +1,62 @@
 
-use common::is_zero_address;
-use proto::pb::evm::erc721::v1::Events as ERC721Transfers;
+use std::collections::{HashMap, HashSet};
+
+use common::{is_zero_address, Address};
+use proto::pb::evm::erc721::v1::{Events as ERC721Transfers, Transfer};
 use proto::pb::evm::erc721::v1::{EventsMetadata, Metadata};
+
+use crate::calls::{batch_token_collection_name, batch_token_collection_symbol, batch_token_uri};
 
 #[substreams::handlers::map]
 fn map_events(erc721_transfers: ERC721Transfers) -> Result<EventsMetadata, substreams::errors::Error> {
     let mut events = EventsMetadata::default();
+    let mints: Vec<Transfer> = get_mints(erc721_transfers.transfers).collect();
 
-    for transfer in erc721_transfers.transfers {
-        // only track mints
-        if !is_zero_address(transfer.from) {
-            continue;
-        }
+    // Collect unique contracts and token IDs
+    let mut contracts: HashSet<Address> = HashSet::new();
+    let mut contracts_by_token_id: HashSet<(Address, String)> = HashSet::new();
+
+    for transfer in &mints {
+        contracts.insert(transfer.contract.clone());
+        contracts_by_token_id.insert((transfer.contract.clone(), transfer.token_id.clone()));
+    }
+
+    // Fetch RPC calls for tokens
+    let contract_vec: Vec<Address> = contracts.iter().cloned().collect();
+    let contract_uri_vec: Vec<(Address, String)> = contracts_by_token_id.into_iter()
+        .map(|(contract, token_id)| (contract, token_id))
+        .collect();
+    let symbols: HashMap<Address, String> = batch_token_collection_symbol(contract_vec.clone());
+    let names: HashMap<Address, String> = batch_token_collection_name(contract_vec);
+    let uris: HashMap<(Address, String), String> = batch_token_uri(contract_uri_vec);
+
+    for transfer in mints {
+        let uri = match uris.get(&(transfer.contract.clone(), transfer.token_id.clone())) {
+            Some(uri) => Some(uri.to_string()),
+            None => None,
+        };
+        let symbol = match symbols.get(&transfer.contract) {
+            Some(symbol) => Some(symbol.to_string()),
+            None => None,
+        };
+        let name = match names.get(&transfer.contract) {
+            Some(name) => Some(name.to_string()),
+            None => None,
+        };
         events.metadatas.push(Metadata {
             contract: transfer.contract.to_vec(),
             token_id: transfer.token_id.to_string(),
-            uri: None,
-            symbol: None,
-            name: None,
+            uri,
+            symbol,
+            name,
         });
     }
 
     Ok(events)
+}
+
+pub fn get_mints<'a>(erc721_transfers: Vec<Transfer>) -> impl Iterator<Item = Transfer> + 'a {
+    erc721_transfers.into_iter().filter(|transfer| {
+        !is_zero_address(transfer.from.to_vec())
+    })
 }
