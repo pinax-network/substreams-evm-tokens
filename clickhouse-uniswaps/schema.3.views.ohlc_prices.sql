fcb4d757f60c596d05917e60f0738a0e6ffb1baf
Denis Carriere <carriere.denis@gmail.com>
2025-05-17 21:16:11 -0400

    update schema

--- clickhouse-uniswaps/schema.3.views.ohlc_prices.sql
+++ clickhouse-uniswaps/schema.3.views.ohlc_prices.sql
@@ -4,7 +4,19 @@ CREATE TABLE IF NOT EXISTS ohlc_prices (
 
     -- pool --
     pool                 String COMMENT 'pool address',
-    protocol             SimpleAggregateFunction(anyLast, LowCardinality(String)),
+    protocol             SimpleAggregateFunction(any, LowCardinality(String)),
+
+    -- token0 erc20 metadata --
+    token0               SimpleAggregateFunction(any, FixedString(42)),
+    decimals0            SimpleAggregateFunction(any, UInt8),
+    symbol0              SimpleAggregateFunction(anyLast, String),
+    name0                SimpleAggregateFunction(anyLast, String),
+
+    -- token1 erc20 metadata --
+    token1               SimpleAggregateFunction(any, FixedString(42)),
+    decimals1            SimpleAggregateFunction(any, UInt8),
+    symbol1              SimpleAggregateFunction(anyLast, String),
+    name1                SimpleAggregateFunction(anyLast, String),
 
     -- swaps --
     open0                AggregateFunction(argMin, Float64, UInt64),
@@ -29,25 +41,40 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS mv_ohlc_prices
 TO ohlc_prices
 AS
 SELECT
-    toStartOfHour(timestamp) AS timestamp,
+    toStartOfHour(s.timestamp) AS timestamp,
 
     -- pool --
-    pool AS pool,
-    any(protocol) AS protocol,
+    s.pool AS pool,
+    any(s.protocol) AS protocol,
+
+    -- token0 erc20 metadata --
+    any(p.token0)           AS token0,
+    any(m0.decimals)        AS decimals0,
+    anyLast(m0.name)        AS name0,
+    anyLast(m0.symbol)      AS symbol0,
+
+    -- token1 erc20 metadata --
+    any(p.token1)           AS token1,
+    any(m1.decimals)        AS decimals1,
+    anyLast(m1.name)        AS name1,
+    anyLast(m1.symbol)      AS symbol1,
 
     -- swaps --
-    argMinState(price, global_sequence)                AS open0,
-    quantileDeterministicState(price, global_sequence) AS quantile0,
-    argMaxState(price, global_sequence)                AS close0,
+    argMinState(s.price, s.global_sequence)                AS open0,
+    quantileDeterministicState(s.price, s.global_sequence) AS quantile0,
+    argMaxState(s.price, s.global_sequence)                AS close0,
 
     -- volume --
-    sum(toUInt256(abs(amount0)))     AS gross_volume0,
-    sum(toUInt256(abs(amount1)))     AS gross_volume1,
-    sum(toInt256(amount0))           AS net_flow0,
-    sum(toInt256(amount1))           AS net_flow1,
+    sum(toUInt256(abs(s.amount0)))     AS gross_volume0,
+    sum(toUInt256(abs(s.amount1)))     AS gross_volume1,
+    sum(toInt256(s.amount0))           AS net_flow0,
+    sum(toInt256(s.amount1))           AS net_flow1,
 
     -- universal --
-    uniqState(tx_from) AS uaw,
+    uniqState(s.tx_from) AS uaw,
     sum(1) AS transactions
-FROM swaps
+FROM swaps AS s
+JOIN pools AS p USING (pool)
+JOIN erc20_metadata AS m0 ON m0.address = p.token0
+JOIN erc20_metadata AS m1 ON m1.address = p.token1
 GROUP BY pool, timestamp;
